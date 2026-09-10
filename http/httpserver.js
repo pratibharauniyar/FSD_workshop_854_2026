0becd43c478f28a84f6df43dcd2e4ffdc1e1a144
pratibharauniyar <pratibharauniyar57@gmail.com>
2026-09-10 16:02:02 +0530

    http server

---
+++ http/httpserver.js
@@ -0,0 +1,10 @@
+const http=require("http");
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{
+        "content-type":"text/plain"
+    });
+    res.end("hello world");
+});
+server.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
